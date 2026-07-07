8e186ebf4a377165885a51f8970f2e61fdaff977
unknown <karindiaye3038@gmail.com>
2026-07-07 11:07:09 +0000

    ajout

--- Frontend/Javascript/script.js
+++ Frontend/Javascript/script.js
@@ -2,51 +2,7 @@
 // PROTECTION DE LA PAGE
 // ============================
 
-sync function loadDashboard() {
 
-    // Vérifier si le token existe
-    const token = localStorage.getItem("token");
-
-    if (!token) {
-        window.location.replace("index.html");
-        return;
-    }
-
-    try {
-
-        const response = await fetch("https://red-products.onrender.com/api/dashboard/kpis", {
-            method: "GET",
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        });
-
-        // Si le token est invalide ou expiré
-        if (response.status === 401 || response.status === 403) {
-
-            localStorage.removeItem("token");
-
-            window.location.replace("index.html");
-
-            return;
-        }
-
-        const data = await response.json();
-
-        document.getElementById("users").innerText = data.users;
-        document.getElementById("hotels").innerText = data.hotels;
-        document.getElementById("messages").innerText = data.messages;
-        document.getElementById("emails").innerText = data.emails;
-
-    } catch (error) {
-
-        console.error(error);
-
-    }
-
-}
-
-loadDashboard();
 // ============================
 // MODAL
 // ============================
@@ -331,13 +287,6 @@ async function editHotel(id) {
     }
 
 }
-// ============================
-// DÉCONNEXION
-// ============================
-
-// ============================
-// DÉCONNEXION
-// ============================
 
 // ============================
 // DÉCONNEXION
