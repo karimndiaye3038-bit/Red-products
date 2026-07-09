753e4562aa8bec1f141dee30ee9d4590b6894d5b
unknown <karindiaye3038@gmail.com>
2026-07-09 13:28:17 +0000

    ajout

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -1,38 +1,22 @@
-const brevo = require("../config/email");
+const SibApiV3Sdk = require('sib-api-v3-sdk');
 
-const sendEmail = async ({ to, subject, html }) => {
+const client = SibApiV3Sdk.ApiClient.instance;
+client.authentications['api-key'].apiKey = process.env.BREVO_API_KEY;
 
-    try {
+const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
 
-        await brevo.transactionalEmails.sendTransacEmail({
-
-            sender: {
-                name: "RED PRODUCT",
-                email: process.env.EMAIL_USER
-            },
-
-            to: [
-                {
-                    email: to
-                }
-            ],
-
-            subject,
-
-            htmlContent: html
-
-        });
-
-        console.log("Email envoyé avec succès");
-
-    } catch (error) {
-
-        console.error("Erreur Brevo :", error);
-
-        throw error;
-
-    }
-
-};
+async function sendEmail({ to, subject, html }) {
+  try {
+    await apiInstance.sendTransacEmail({
+      sender: { email: "tonemail@domaine.com", name: "RED PRODUCT" },
+      to: [{ email: to }],
+      subject,
+      htmlContent: html
+    });
+    console.log("Email envoyé à", to);
+  } catch (error) {
+    console.error("Erreur envoi email:", error);
+  }
+}
 
 module.exports = sendEmail;
