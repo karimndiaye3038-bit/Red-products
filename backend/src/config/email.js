29ce228ddb0e0c315059e005bd58aa400cd4f498
unknown <karindiaye3038@gmail.com>
2026-07-11 20:13:57 +0000

    ajout du brevo

--- backend/src/config/email.js
+++ backend/src/config/email.js
@@ -1,5 +1,7 @@
 const { BrevoClient } = require("@getbrevo/brevo");
 
+console.log("BREVO_API_KEY =", process.env.BREVO_API_KEY);
+
 const brevo = new BrevoClient({
     apiKey: process.env.BREVO_API_KEY,
 });
